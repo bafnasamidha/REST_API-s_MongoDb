34324a529a89aaf638ca526f6971cef59bc63d66
sam <bafnasamidha@gmail.com>
2024-09-24 14:17:51 +0530

    commit after following mvc architecture

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require("express");
 
 const app = express();
 
-const {userRouter}=require("./routes/user");
+const userRouter = require("./routes/user");
 
-const {connectMongoDb}=require("./connection");
+const { connectMongoDb } = require("./connection");
 
 connectMongoDb("mongodb://127.0.0.1:27017/rest_api");
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use("/user",userRouter);
+app.use("/user", userRouter);
 
 app.listen(8000, () => {
   console.log("server started");
